774b32fa4d3c17d3cb8ada1375b9f0e392575024
Loan Robert <loan.rbrt@gmail.com>
2016-09-23 18:00:37 +0200

    associations, needs validations withs friends

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,3 +1,9 @@
 class Credit < ApplicationRecord
   belongs_to :account
+
+  # validates :creditor, presence: true
+  # validates :debtor, presence: true
+  # validates :debtor, exclusion: :creditor
+  validates :amount, presence: true
+
 end
