c0393aa3e59c673c537420984259eb75275b8e13
Loan Robert <loan.rbrt@gmail.com>
2016-09-23 16:24:15 +0200

    all models

---
+++ db/migrate/20160923141008_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :transactions do |t|
+      t.references :account, foreign_key: true
+      t.date :date
+      t.float :amount
+      t.string :creditor
+      t.text :comment
+
+      t.timestamps
+    end
+  end
+end
