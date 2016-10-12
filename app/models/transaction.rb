4f3c7707377ec997728dcae66590a1cde63f414f
Loan Robert <loan.rbrt@gmail.com>
2016-10-12 14:43:37 +0200

    some new inputs, pb with dates

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :board
 
-  # validates :creditor, presence: true
+  validates :creditor, presence: true
   validates :amount, presence: true
 
 end
