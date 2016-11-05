22768fea2a410fa4484272aae8ea476cac572032
loan robert <loan.rbrt@gmail.com>
2016-11-05 15:52:24 +0100

    decimals done

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,9 +1,13 @@
 class Credit < ApplicationRecord
   belongs_to :board
+  has_one :creditor, through: :board, class_name: "Friend"
+  has_one :debtor, through: :board, class_name: "Friend"
 
-  # validates :creditor, presence: true
-  # validates :debtor, presence: true
-  # validates :debtor, exclusion: :creditor
-  validates :amount, presence: true
+
+  validates :creditor, presence: true
+  validates :debtor, presence: true
+  # validates :debtor, uniqueness: { scope: :creditor }
+  validates :debtor, exclusion: { in: :creditor, message: "Cannot be in debt of its own credit!"
+  validates :amountint, presence: true
 
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,6 @@
 class Friend < ApplicationRecord
   belongs_to :board
+  # has_many :transactions, through: :board
 
   validates :name, presence: true
 
