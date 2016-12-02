494b3aad8b2b8b35be966a663440e34682d16476
loan robert <loan.rbrt@gmail.com>
2016-12-02 22:29:37 +0100

    bug fixes on js sorting with regex and others

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,4 +1,4 @@
-// calculate total of a board
+// calculate total of a board when loading page
 function sumBoard(boardId) {
   var sum = 0;
   $('#transactions_content_' + boardId + ' .transaction .transaction_amount').each(function() {
@@ -8,6 +8,7 @@ function sumBoard(boardId) {
   $('#transactions_sum_' + boardId).html(sum);
 };
 
+// add the new transaction value to the total without adding up all elements on the page
 function addTransaction(boardId, childToAdd) {
   var totalAfter = (Number($('#transactions_sum_' + boardId).text()) * 100 + ($('#transactions_content_' + boardId + ' .transaction:nth-child(' + childToAdd + ') .transaction_amount').text() * 100)) / 100
   $('#transactions_sum_' + boardId).html(totalAfter);
